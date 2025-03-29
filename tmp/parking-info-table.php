4de0d1201f05f427892044a0a092596b613b2093
hama0115 <oh.akibee1231@gmail.com>
2025-03-30 02:51:24 +0900

    パーキングメーター情報をスタイリング

--- tmp/parking-info-table.php
+++ tmp/parking-info-table.php
@@ -8,23 +8,25 @@
       ?>
       <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
       <?php endif; ?>
-      <?php if(get_sub_field('time')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
-      <dl>
-        <dt>【時間】</dt>
-        <dd><?php the_sub_field('time'); ?></dd>
-      </dl>
-      <?php endif; ?>
-      <?php if(get_sub_field('system')): ?>
-      <dl>
-        <dt>【方式】</dt>
-        <dd><?php the_sub_field('system'); ?></dd>
-      </dl>
-      <?php endif; ?>
-      <?php if(get_sub_field('the-number-of-field')): ?>
-      <dl>
-        <dt>【枠数】</dt>
-        <dd><?php the_sub_field('the-number-of-field'); ?></dd>
-      </dl>
+      <div class="parking-text-container">
+        <?php if(get_sub_field('time')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
+        <dl>
+          <dt>【時間】</dt>
+          <dd><?php the_sub_field('time'); ?></dd>
+        </dl>
+        <?php endif; ?>
+        <?php if(get_sub_field('system')): ?>
+        <dl>
+          <dt>【方式】</dt>
+          <dd><?php the_sub_field('system'); ?></dd>
+        </dl>
+        <?php endif; ?>
+        <?php if(get_sub_field('the-number-of-field')): ?>
+        <dl>
+          <dt>【枠数】</dt>
+          <dd><?php the_sub_field('the-number-of-field'); ?></dd>
+        </dl>
+      </div>
     </div>
     <?php endif; ?>
   <?php endwhile; ?>
