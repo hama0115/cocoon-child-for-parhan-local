772b3ad8ee8971627e85a9b83655486a3f14b754
hama0115 <oh.akibee1231@gmail.com>
2025-03-30 02:29:43 +0900

    wordpressの追加CSSを削除して子テーマのstyle.cssに移行

--- tmp/parking-info-table.php
+++ tmp/parking-info-table.php
@@ -1,31 +1,31 @@
 <!-- ACF「パーキングメーター情報」を表示 -->
 <?php if(have_rows('meter-info')): ?>
   <?php while(have_rows('meter-info')): the_row(); ?>
-    <?php //対象のサブフィールド(画像)が存在する場合に出力
-    $image = get_sub_field('parkingmeter-photo');
-    if(!empty($image)):
-    ?>
+    <div class="parking-info-container">
+      <?php //対象のサブフィールド(画像)が存在する場合に出力
+      $image = get_sub_field('parkingmeter-photo');
+      if(!empty($image)):
+      ?>
       <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-    <?php endif; ?>
-    <?php if(get_sub_field('time')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
-    <dl>
-      <dt>【時間】</dt>
-      <dd><?php the_sub_field('time'); ?></dd>
-    </dl>
-    <?php endif; ?>
-
-    <?php if(get_sub_field('system')): ?>
-    <dl>
-      <dt>【方式】</dt>
-      <dd><?php the_sub_field('system'); ?></dd>
-    </dl>
-    <?php endif; ?>
-
-    <?php if(get_sub_field('the-number-of-field')): ?>
-    <dl>
-      <dt>【枠数】</dt>
-      <dd><?php the_sub_field('the-number-of-field'); ?></dd>
-    </dl>
+      <?php endif; ?>
+      <?php if(get_sub_field('time')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
+      <dl>
+        <dt>【時間】</dt>
+        <dd><?php the_sub_field('time'); ?></dd>
+      </dl>
+      <?php endif; ?>
+      <?php if(get_sub_field('system')): ?>
+      <dl>
+        <dt>【方式】</dt>
+        <dd><?php the_sub_field('system'); ?></dd>
+      </dl>
+      <?php endif; ?>
+      <?php if(get_sub_field('the-number-of-field')): ?>
+      <dl>
+        <dt>【枠数】</dt>
+        <dd><?php the_sub_field('the-number-of-field'); ?></dd>
+      </dl>
+    </div>
     <?php endif; ?>
   <?php endwhile; ?>
 <?php endif; ?>
